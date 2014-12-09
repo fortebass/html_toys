3f6acc25b59114dd8b59257f119663e3b5248d99
forte <forte.bass814@gmail.com>
2014-12-09 22:39:48 +0800

    game client using box2d, server using websocket

---
+++ dump-way-to-water/server/clientTest.js
@@ -0,0 +1,25 @@
+var WebSocket = require('ws')
+  , websocket = new WebSocket('ws://192.168.33.5:8005');
+websocket.on('open', function() {
+    var play = {
+            state: "login",//"restart","playing","fail","succeed"
+            player:{
+                playing:0,
+                id:"003",
+                takeTool:"tool03",
+                toolPosX:10,
+                toolPosY:10
+            },
+            level:1
+        }
+    websocket.send(JSON.stringify(play));
+});
+websocket.on('message', function(message) {
+    console.log('received: %s', message);
+});
+websocket.on('close', function(message) {
+    console.log('close : received: %s', message);
+});
+websocket.on('error', function(message) {
+    console.log('error : received: %s', message);
+});
